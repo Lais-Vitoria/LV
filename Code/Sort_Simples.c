f721b4e89f7202ec266b989106c2f211b5f1cd44
Lais Vitoria <152716115+Lais-Vitoria@users.noreply.github.com>
2024-07-02 11:58:04 -0300

    erro corrigido

--- Code/Sort_Simples.c
+++ Code/Sort_Simples.c
@@ -12,33 +12,27 @@ int main ()
     Orig2 = Num2;
     Orig3 = Num3;
 
-    if (Num1 < Num2 )
+    if (Num1 > Num2) 
     {
         Aux = Num1;
         Num1 = Num2;
         Num2 = Aux;
-        if (Num2 > Num3)
-        {
-            Aux = Num2;
-            Num2 = Num3;
-            Num3 = Aux;
-        }
-        if (Num1 > Num2)
-        {
-            Aux = Num1;
-            Num1 = Num2;
-            Num2 = Aux;
-        }
-        if (Num2 > Num3)
-        {
-            Aux = Num2;
-            Num2 = Num3;
-            Num3 = Aux;
-        }
-        printf("%d\n", Num1);
-        printf("%d\n", Num2);
-        printf("%d\n", Num3);
     }
+    if (Num1 > Num3) 
+    {
+        Aux = Num1;
+        Num1 = Num3;
+        Num3 = Aux;
+    }
+    if (Num2 > Num3) 
+    {
+        Aux = Num2;
+        Num2 = Num3;
+        Num3 = Aux;
+    }
+    printf("%d\n", Num1);
+    printf("%d\n", Num2);
+    printf("%d\n", Num3);
     printf("\n");
     printf("%d\n", Orig1);
     printf("%d\n", Orig2);
