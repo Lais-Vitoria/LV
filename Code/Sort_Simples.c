e776e86ca054bd56ee7dea85527cd91956688c65
Lais Vitoria <152716115+Lais-Vitoria@users.noreply.github.com>
2024-07-02 11:42:11 -0300

    New code

---
+++ Code/Sort_Simples.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main ()
+{
+    int Num1, Num2, Num3, Aux, Orig1, Orig2, Orig3;
+
+    scanf("%d", &Num1);
+    scanf("%d", &Num2);
+    scanf("%d", &Num3);
+
+    Orig1 = Num1;
+    Orig2 = Num2;
+    Orig3 = Num3;
+
+    if (Num1 < Num2 )
+    {
+        Aux = Num1;
+        Num1 = Num2;
+        Num2 = Aux;
+        if (Num2 > Num3)
+        {
+            Aux = Num2;
+            Num2 = Num3;
+            Num3 = Aux;
+        }
+        if (Num1 > Num2)
+        {
+            Aux = Num1;
+            Num1 = Num2;
+            Num2 = Aux;
+        }
+        if (Num2 > Num3)
+        {
+            Aux = Num2;
+            Num2 = Num3;
+            Num3 = Aux;
+        }
+        printf("%d\n", Num1);
+        printf("%d\n", Num2);
+        printf("%d\n", Num3);
+    }
+    printf("\n");
+    printf("%d\n", Orig1);
+    printf("%d\n", Orig2);
+    printf("%d\n", Orig3);
+    
+}
