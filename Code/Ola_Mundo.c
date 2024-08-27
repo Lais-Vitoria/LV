f18d38750e503e4ca3c243ab0e74154a0cf85a10
Lais Vitoria Da Silva Cruz <laisvitoriadasilvacruz@gmail.com>
2024-08-27 17:25:56 -0300

    Introdução da lista de idiomas

---
+++ Code/Ola_Mundo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main ()
+{
+    int Introducao;
+
+    printf("Escolha seu idioma\n");
+
+    Printf("|----------------------------------------------------------|");
+    printf("|  1 - Alemão       |  11 - Grego      |  21 - Polonês     |");
+    printf("|  2 - Árabe        |  12 - Hebraico   |  22 - Português   |");
+    printf("|  3 - Bengali      |  13 - Hindi      |  23 - Romeno      |");
+    printf("|  4 - Chinês       |  14 - Holandês   |  24 - Russo       |");
+    printf("|  5 - Coreano      |  15 - Húngaro    |  25 - Sueco       |");
+    printf("|  6 - Dinamarquês  |  16 - Indonésio  |  26 - Swahili     |");
+    printf("|  7 - Espanhol     |  17 - Inglês     |  27 - Tailandês   |");
+    printf("|  8 - Filipino     |  18 - Italiano   |  28 - Tcheco      |");
+    printf("|  9 - Finlandês    |  19 - Japonês    |  29 - Turco       |");
+    printf("|  10 - Francês     |  20 - Norueguês  |  30 - Vietnamita  |");
+    Printf("|----------------------------------------------------------|");
+
+    
+}
